72add72a163687ab3ce176bfbfe82cb6c17a2568
Vishal Rai <v1709r@gmail.com>
2025-05-14 02:55:46 +0530

    Completing Arrays Swiftly

--- Arrays/IsSorted.java
+++ Arrays/IsSorted.java
@@ -1,7 +1,14 @@
 class IsSorted {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        
- 
+        int[] arr = {10, 9, 8, 7, 6, 5 ,4 ,3 ,2, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.print(isSorted(arr));
+   }
+   static Boolean isSorted(int [] arr) {
+        for(int i = 1;  i < arr.length; i++) {
+            if(arr[i - 1] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
    }
 }
---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,31 @@
+class SecondLargest {
+    public static void main(String [] args) {
+        int [] arr = {5,8, 12, 12};
+        // int [] arr = {10, 10, 10};
+        // int [] arr = {10, 5, 8, 20};
+        // int [] arr = {20, 10, 20, 8, 12};
+        System.out.println(secondLargest(arr));
+    }
+    static int secondLargest(int [] arr) {
+        int largest = arr[0];
+        int ans = -1;
+        for(int i : arr) {
+            if(largest < i) {
+                ans = largest;
+                largest = i;
+            }
+            if (ans < i && i < largest) {
+                ans = i;
+            }
+            // if(largest < i) {
+            //    ans = largest;
+            //    largest = i;
+            // } else {
+            //     if(ans < i) {
+
+            //     }
+            // }
+        }
+        return ans;
+    }
+}
